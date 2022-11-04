966476f83e257a66d8dbef5970c8463137854a7e
Click1N <111238061+Click1N@users.noreply.github.com>
2022-11-04 18:02:18 +0100

    updated main

--- main.py
+++ main.py
@@ -1,16 +1,94 @@
-# This is a sample Python script.
+import random
+from dataclasses import dataclass, field
+from collections import defaultdict
+from typing import TypeAlias, TypedDict
 
-# Press ⌃R to execute it or replace it with your code.
-# Press Double ⇧ to search everywhere for classes, files, tool windows, actions, and settings.
+from faker import Faker
+
+T_GROUP_NAME: TypeAlias = str
+T_GROUP_NAMES: TypeAlias = list[T_GROUP_NAME]
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press ⌘F8 to toggle the breakpoint.
+class Human(TypedDict):
+    name: str
+    group: T_GROUP_NAME
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+T_HUMANS: TypeAlias = list[Human]
 
-# See PyCharm help at https://www.jetbrains.com/help/pycharm/
+
+@dataclass
+class DataProvider:
+    _faker: Faker = field(default_factory=Faker)
+
+    def _generate_group_names(
+        self,
+        amount: int = 10,
+    ) -> T_GROUP_NAMES:
+        return [self._faker.unique.company() for _ in range(amount)]
+
+    def _generate_human(self, group_name: T_GROUP_NAME) -> Human:
+        return Human(
+            name=self._faker.unique.first_name(),
+            group=group_name,
+        )
+
+    def _generate_humans(
+        self, groups: T_GROUP_NAMES, amount_of_humans: int
+    ) -> T_HUMANS:
+        members = []
+        for _ in range(amount_of_humans):
+            group_name = random.choice(groups)
+            group_member = self._generate_human(group_name=group_name)
+            members.append(group_member)
+
+        return members
+
+    def generate_group_members(
+        self,
+        amount_of_groups: None | int = None,
+        amount_of_humans: None | int = None,
+    ) -> T_HUMANS:
+        amount_of_groups = amount_of_groups or random.randint(5, 10)
+        amount_of_humans = amount_of_humans or random.randint(3, 30)
+
+        _groups = self._generate_group_names(amount=amount_of_groups)
+        return self._generate_humans(groups=_groups, amount_of_humans=amount_of_humans)
+
+
+def organize_data(humans: T_HUMANS) -> dict[str, list]:
+    """
+    Organize data in way, useful for further processing.
+    At this stage not allowed to make output string.
+    """
+    org_data: dict[str, list] = defaultdict(list)
+
+    for i in humans:
+        org_data[i["group"]].append(i["name"])
+    return org_data
+
+
+def get_formatted_output(data: dict) -> str:
+    """
+    Get output string. That can be used to print in console.
+    """
+    output = [
+        f'The group "{key}" has next {len(data[key])} participants: {", ".join(i for i in data[key])}'
+        for key in sorted(data)
+    ]
+    return "\n".join(output)
+
+
+def main():
+    """
+    You have a list of humans. Every human have "name" and "group".
+    Your task is to show all groups, with amount and names of members of each group.
+    """
+    group_members = DataProvider().generate_group_members()
+    organized_data = organize_data(humans=group_members)
+    output = get_formatted_output(data=organized_data)
+    print(output)
+
+
+if __name__ == "__main__":
+    main()
